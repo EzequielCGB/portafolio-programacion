a6010c81ad019d5ba7d3d882d9e0bcb5bb59b253
EzequielGBC <ezequielcepeda93@gmail.com>
2026-04-12 23:08:15 -0300

    Mejora, validacion de operadores

--- ejercicios-c/calculadora-funciones.c
+++ ejercicios-c/calculadora-funciones.c
@@ -22,50 +22,54 @@ int main (){
     char operacion;
     float num1, num2, resultado;
     do {
-    menu_de_inicio ();//Llamo a la funcion void 
+        menu_de_inicio ();//Llamo a la funcion void 
 
-    scanf(" %c", &operacion);
+        scanf(" %c", &operacion);
+         if (operacion != '+' && operacion != '-' && operacion != '/' && operacion != '*'){
+            printf("Operacion no reconocida");
+         } else {
 
-    printf("\nIngrese el primer numero\n");
-    scanf("%f", &num1);
-    printf("\nIngrese el segundo numero\n");
-    scanf("%f", &num2);
+            printf("\nIngrese el primer numero\n");
+            scanf("%f", &num1);
+            printf("\nIngrese el segundo numero\n");
+            scanf("%f", &num2);
 
-    switch (operacion) {
-            case '+':
-                resultado = suma (num1 , num2);
-                printf("\nEl resultado de su suma es: %.2f", resultado);
-                break;
-            case '-':
-                resultado = resta (num1 , num2);
-                printf("\nEl resultado de su resta es: %.2f", resultado);
-                break;
-            case '/':
-                if(num2 == 0){
-                    printf("\nError, cualquier numero dividido por cero da infinito");
-                } else {
-                    resultado = division (num1 , num2);
-                    printf("\nEl resultado de su division es: %.2f", resultado);
-                }
-                break;
-            case '*':
-                resultado = producto (num1 , num2);
-                printf("\nEl resultado de su multiplicacion es: %.2f", resultado);
-                break;
+            switch (operacion) {
+                    case '+':
+                        resultado = suma (num1 , num2);
+                        printf("\nEl resultado de su suma es: %.2f", resultado);
+                        break;
+                    case '-':
+                        resultado = resta (num1 , num2);
+                        printf("\nEl resultado de su resta es: %.2f", resultado);
+                        break;
+                    case '/':
+                        if(num2 == 0){
+                            printf("\nError, cualquier numero dividido por cero da infinito");
+                        } else {
+                            resultado = division (num1 , num2);
+                            printf("\nEl resultado de su division es: %.2f", resultado);
+                        }
+                        break;
+                    case '*':
+                        resultado = producto (num1 , num2);
+                        printf("\nEl resultado de su multiplicacion es: %.2f", resultado);
+                        break;
 
-            default:
-                printf("El programa no reconoce esos valores");
-                break;
-    }
-    printf("\nDesea volver a usar la calculadora: Marque 1 para si y 0 para No\n");
-    scanf("%d", &opcion);
-        if (opcion == 0){
-            printf("\nCerrando calculadora, muchas gracias");
-        } else if (opcion != 0 && opcion != 1){
-            printf ("\n Valores no reconocidos");
+                    default:
+                        printf("El programa no reconoce esos valores");
+                        break;
+            }
         }
-        
-    } while (opcion == 1);
+        printf("\nDesea volver a usar la calculadora: Marque 1 para si y 0 para No\n");
+        scanf("%d", &opcion);
+            if (opcion == 0){
+                printf("\nCerrando calculadora, muchas gracias");
+            } else if (opcion != 0 && opcion != 1){
+                printf ("\n Valores no reconocidos");
+            }
+            
+        } while (opcion == 1);
 
     return 0;
 }
